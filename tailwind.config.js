29da8ab10f355e189a5667d47e70ea4c4d2ce4c0
vinu tv <vinutv91@gmail.com>
2021-11-06 04:11:22 +0530

    Add  professional skills illustrations

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,21 +13,21 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        inter:
-          'Inter, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";',
+        montserrat:
+          'Montserrat, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";',
       },
       colors: {
         primary: {
-          50: "#FDFAF7",
-          100: "#FCEEE2",
-          200: "#F9DBC1",
-          300: "#F7BD8D",
-          400: "#F6904D",
-          500: "#FD6400",
-          600: "#E05900",
-          700: "#C75000",
-          800: "#9E3F00",
-          900: "#803300",
+          50: "#FFFCF3",
+          100: "#FFD761",
+          200: "#FFF3BF",
+          300: "#FFEBA0",
+          400: "#FFE488",
+          500: "#FFD761",
+          600: "#DBB146",
+          700: "#B78E30",
+          800: "#936D1E",
+          900: "#7A5512",
         },
         secondary: {
           50: "#F6F1FD",
