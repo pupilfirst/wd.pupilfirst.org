ee5940c73496ec2f59804f0e4f6155f2d9054065
Reena Singh <reena@pupilfirst.org>
2022-02-08 11:49:53 +0530

    Update index.js
    Change arrow colour between WD301-401

--- pages/index.js
+++ pages/index.js
@@ -780,7 +780,7 @@ export default function Home() {
                       width="16"
                       height="16"
                       fill="currentColor"
-                      className="text-indigo-700"
+                      className="text-green-700"
                       viewBox="0 0 16 16"
                     >
                       <path
