e93be67bc6c0feaa1642e9721b3c614aa3e78afc
Bodhish Thomas <bodhish@gmail.com>
2022-06-05 08:54:34 +0530

    Add build dir

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 module.exports = {
+  distDir: "build",
   async redirects() {
     return [
       {
-        source: '/apply',
-        destination: 'http://apply.pupilfirst.org/',
+        source: "/apply",
+        destination: "http://apply.pupilfirst.org/",
         permanent: true,
       },
-    ]
+    ];
   },
-}
+};
