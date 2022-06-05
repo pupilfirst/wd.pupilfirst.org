07fdcc9ff7d8744faae5fc417be245721c739b04
Vinu <vinutv91@gmail.com>
2022-06-06 00:48:27 +0530

    Add logo cloud for early adopters (#14)

--- next.config.js
+++ next.config.js
@@ -2,10 +2,10 @@ module.exports = {
   async redirects() {
     return [
       {
-        source: '/apply',
-        destination: 'http://apply.pupilfirst.org/',
+        source: "/apply",
+        destination: "http://apply.pupilfirst.org/",
         permanent: true,
       },
-    ]
+    ];
   },
-}
+};
