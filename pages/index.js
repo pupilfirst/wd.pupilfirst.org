92b6d702c15012114d2375f55d869d43effd5b15
Bodhish Thomas <bodhish@gmail.com>
2022-06-06 10:50:18 +0530

    Replace crossorigin with crossOrigin

--- pages/index.js
+++ pages/index.js
@@ -63,7 +63,7 @@ export default function Home(props) {
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
         <link rel="icon" href="/favicon.ico" type="image/x-icon" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800&family=Source+Code+Pro&display=swap"
           rel="stylesheet"
