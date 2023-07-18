fb4654621b902efdf3b27dbdb8b02b5a1f6f3b4c
vinu tv <vinutv91@gmail.com>
2023-07-18 16:21:31 +0530

    Update content learner-centered classroom section

--- pages/index.js
+++ pages/index.js
@@ -548,7 +548,7 @@ export default function Home(props) {
           darkBackground="true"
         >
           <p className="text-white mt-4 md:text-lg w-full md:w-4/5">
-            You will be a part of this cohort having students across 9 states in
+            You will be a part of this cohort having students across 8 states in
             India to learn via Learner-centred Classrooms following National
             Educational Policy 2020 guidelines (NEP 2020).
           </p>
